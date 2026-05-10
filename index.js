401df4a8e46b643e7979da874ea0df11c074d994
Md Royet <rroyet8@gmail.com>
2026-05-10 20:11:28 +0600

    add MongoDB client setup and update dependencies

--- index.js
+++ index.js
@@ -2,9 +2,40 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+dns.setDefaultResultOrder("ipv4first");
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://simpleCRUDuser:AblGEows18FdH64r@cluster0.b83kszp.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const run = async () => {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+};
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Simple CRUD server is running");
